533fa836962ce4141c6937a898f0180bdedef549
Bannon Tanner <bannonta@amazon.com>
2022-12-06 08:22:48 -0600

    switched to regular authenticator to remove sign up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import { useEffect, useState } from "react";
 import {
+  Authenticator,
   Button,
   Divider,
   Flex,
+  Heading,
   Link,
   Radio,
   RadioGroupField,
   Text,
   TextAreaField,
   TextField,
+  useTheme,
   View,
-  withAuthenticator,
 } from "@aws-amplify/ui-react";
 import { API, graphqlOperation } from "aws-amplify";
 import { createFeedback } from "./graphql/mutations";
 
-function App({ signOut, user }) {
-  // remove withAuthenticator and login functionality for live version
-  const username = user.getUsername();
+// remove for production
+const formFields = {
+  confirmVerifyUser: {
+    confirmation_code: {
+      label: "New Label",
+      placeholder: "Enter your Confirmation Code:",
+      isRequired: false,
+    },
+  },
+};
 
+const components = {
+  VerifyUser: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+
+  ConfirmVerifyUser: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+function App() {
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
@@ -79,80 +125,91 @@ function App({ signOut, user }) {
 
   // TODO: move form to own component
   return (
-    <Flex
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      alignContent="center"
-      gap="0.5rem"
-      margin="1rem"
+    <Authenticator
+      formFields={formFields}
+      components={components}
+      hideSignUp={true}
     >
-      {/* remove username/signout for live version */}
-      <Text as="h1" variant="heading">
-        User: {username}
-      </Text>
-      <Button onClick={signOut}>Sign Out</Button>
-      {/* end remove username/signout for live version */}
-      {!submitted ? (
-        <>
-          <Text>Github Issue URL: </Text>
-          <Link href={issueUrl}>{issueUrl}</Link>
-          <View as="form">
-            <Flex direction="column" gap="1rem">
-              <TextField
-                descriptiveText="Enter a valid email address. AWS Amplify will use this to contact you if we have any follow-up questions."
-                placeholder="Email"
-                label="Email (optional)"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Divider orientation="horizontal" />
-              <RadioGroupField
-                label="Are you satisfied with the resolution of your issue?"
-                name="satisfied"
-                direction="row"
-                value={response}
-                onChange={(e) => setResponse(e.target.value)}
-              >
-                <Radio value="yes">Yes</Radio>
-                <Radio value="no">No</Radio>
-              </RadioGroupField>
-              <RadioGroupField
-                label={`How likely are you to recommend ${repo} to a friend?`}
-                direction="row"
-                name="rating"
-                value={rating}
-                onChange={(e) => setRating(e.target.value)}
-              >
-                <Radio value="1">1</Radio>
-                <Radio value="2">2</Radio>
-                <Radio value="3">3</Radio>
-                <Radio value="4">4</Radio>
-                <Radio value="5">5</Radio>
-              </RadioGroupField>
-              <Divider orientation="horizontal" />
-              <TextAreaField
-                label="Do you have any feedback on the resolution of your issue?"
-                placeholder="Resolution Feedback"
-                onChange={(e) => setResolutionFeedback(e.target.value)}
-              />
-              <Divider orientation="horizontal" />
-              <TextAreaField
-                label="Do you have any feedback about Amplify in general?"
-                placeholder="Amplify Feedback"
-                onChange={(e) => setAmplifyFeedback(e.target.value)}
-              />
-              <Divider orientation="horizontal" />
-              <Button onClick={submitFeedback} variation="primary">
-                Submit
-              </Button>
-            </Flex>
-          </View>
-        </>
-      ) : (
-        <Text>Thank you for your feedback! You can now close this window.</Text>
+      {({ user, signOut }) => (
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          alignContent="center"
+          gap="0.5rem"
+          margin="1rem"
+        >
+          {/* remove username/signout for live version */}
+          <Text as="h1" variant="heading">
+            User: {user.getUsername()}
+          </Text>
+          <Button onClick={signOut}>Sign Out</Button>
+          {/* end remove username/signout for live version */}
+          {!submitted ? (
+            <>
+              <Text>Github Issue URL: </Text>
+              <Link href={issueUrl}>{issueUrl}</Link>
+              <View as="form">
+                <Flex direction="column" gap="1rem">
+                  <TextField
+                    descriptiveText="Enter a valid email address. AWS Amplify will use this to contact you if we have any follow-up questions."
+                    placeholder="Email"
+                    label="Email (optional)"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Divider orientation="horizontal" />
+                  <RadioGroupField
+                    label="Are you satisfied with the resolution of your issue?"
+                    name="satisfied"
+                    direction="row"
+                    value={response}
+                    onChange={(e) => setResponse(e.target.value)}
+                  >
+                    <Radio value="yes">Yes</Radio>
+                    <Radio value="no">No</Radio>
+                  </RadioGroupField>
+                  <RadioGroupField
+                    label={`How likely are you to recommend ${repo} to a friend?`}
+                    direction="row"
+                    name="rating"
+                    // labelPosition="top"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                  >
+                    <Radio value="1">1</Radio>
+                    <Radio value="2">2</Radio>
+                    <Radio value="3">3</Radio>
+                    <Radio value="4">4</Radio>
+                    <Radio value="5">5</Radio>
+                  </RadioGroupField>
+                  <Divider orientation="horizontal" />
+                  <TextAreaField
+                    label="Do you have any feedback on the resolution of your issue?"
+                    placeholder="Resolution Feedback"
+                    onChange={(e) => setResolutionFeedback(e.target.value)}
+                  />
+                  <Divider orientation="horizontal" />
+                  <TextAreaField
+                    label="Do you have any feedback about Amplify in general?"
+                    placeholder="Amplify Feedback"
+                    onChange={(e) => setAmplifyFeedback(e.target.value)}
+                  />
+                  <Divider orientation="horizontal" />
+                  <Button onClick={submitFeedback} variation="primary">
+                    Submit
+                  </Button>
+                </Flex>
+              </View>
+            </>
+          ) : (
+            <Text>
+              Thank you for your feedback! You can now close this window.
+            </Text>
+          )}
+        </Flex>
       )}
-    </Flex>
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
